f3f928c370a7ce76e1cc5934a8d8cc92171a77f6
Kenta Tanaka <tanaka@mana.bi>
2021-10-27 13:43:36 +0900

    Using Invoke-WebRequest rather than curl.

--- jamovi/boot_jamovi_only.ps1
+++ jamovi/boot_jamovi_only.ps1
@@ -6,10 +6,15 @@ Set-WinUILanguageOverride -Language ja-JP
 Set-WinCultureFromLanguageListOptOut -OptOut $False
 Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
 
-$program = "curl"
-$argument = "-L https://www.jamovi.org/downloads/jamovi-2.2.2.0-win64.exe --output C:\users\WDAGUtilityAccount\Downloads\jamovi-2.2.2.0-win64.exe"
+#$program = "curl"
+#$argument = "-L https://www.jamovi.org/downloads/jamovi-2.2.2.0-win64.exe --output C:\users\WDAGUtilityAccount\Downloads\jamovi-2.2.2.0-win64.exe"
 
-Start-Process -FilePath $program -ArgumentList $argument -NoNewWindow -Wait
+#Start-Process -FilePath $program -ArgumentList $argument -NoNewWindow -Wait
+
+
+$ProgressPreference = "SilentlyContinue"
+Invoke-WebRequest -Uri "https://www.jamovi.org/downloads/jamovi-2.2.2.0-win64.exe" -OutFile "C:\users\WDAGUtilityAccount\Downloads\jamovi-2.2.2.0-win64.exe"
+
 
 $program = "C:\users\WDAGUtilityAccount\Downloads\jamovi-2.2.2.0-win64.exe"
 $argument = "/S"
