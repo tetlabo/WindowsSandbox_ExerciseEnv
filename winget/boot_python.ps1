58139b69eb3d99a4d3f16dafa4a9640b78573c1b
Kenta Tanaka <tanaka@mana.bi>
2022-07-26 18:18:33 +0900

    add Microsoft UI XAML package.

--- winget/boot_python.ps1
+++ winget/boot_python.ps1
@@ -12,6 +12,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.4.2011-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage -Path https://raw.githubusercontent.com/ltl-manabi/WindowsSandbox_ExerciseEnv/main/winget/Microsoft.UI.Xaml.2.7.appx
 
 Invoke-Expression -Command "echo Y | winget install --id Google.Chrome"
 
