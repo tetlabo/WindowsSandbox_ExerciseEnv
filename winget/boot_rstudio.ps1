56fdf88341590d05a9443246f770abf2d3d0ae95
Kenta Tanaka <tanaka@mana.bi>
2022-11-04 18:32:56 +0900

    update 20221104.

--- winget/boot_rstudio.ps1
+++ winget/boot_rstudio.ps1
@@ -11,10 +11,15 @@ Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage -Path https://raw.githubusercontent.com/ltl-manabi/WindowsSandbox_ExerciseEnv/main/winget/Microsoft.UI.Xaml.2.7.appx
-Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.4.2011-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage -Path https://raw.githubusercontent.com/tetlabo/WindowsSandbox_ExerciseEnv/main/winget/Microsoft.UI.Xaml.2.7.appx
+Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.4.2161-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-Invoke-Expression -Command "echo Y | winget install --id Google.Chrome"
+Invoke-Expression -Command "echo Y | winget install \"App Installer\" -s msstore"
+
+$program = "winget"
+$argument = "install --id Google.Chrome"
+
+Start-Process -FilePath $program -ArgumentList $argument -NoNewWindow -Wait
 
 
 $program = "winget"
