3d7ddad02c4d87ece7e9b81134cfe51f34d8fb1c
Kenta Tanaka <tanaka@mana.bi>
2022-11-18 12:30:01 +0900

    fix app installer version-03.

--- winget/boot_rstudio.ps1
+++ winget/boot_rstudio.ps1
@@ -12,9 +12,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage -Path https://raw.githubusercontent.com/tetlabo/WindowsSandbox_ExerciseEnv/main/winget/Microsoft.UI.Xaml.2.7.appx
-Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.4.3132-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-Invoke-Expression -Command "echo Y | winget install \`"App Installer\`" -s msstore"
+#Invoke-Expression -Command "echo Y | winget install \`"App Installer\`" -s msstore"
 
 #Invoke-Expression -Command "echo Y | winget install --id Google.Chrome"
 
